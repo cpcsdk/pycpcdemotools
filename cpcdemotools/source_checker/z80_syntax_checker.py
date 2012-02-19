0bda917bea37170bd89541f10e62291a69a16b46
Krusty/Benediction <krusty/cpcscene.com>
2012-02-19 22:06:46 +0100

    [z80 parsing] Use an error format similar to GCC.

--- cpcdemotools/source_checker/z80_syntax_checker.py
+++ cpcdemotools/source_checker/z80_syntax_checker.py
@@ -35,7 +35,7 @@ RE_LOAD_REGISTER_SYNTAX = re.compile('\s*ld\s+(%s|%s)\s*,?\s*(;.*)?$' \
 # Regex for labels
 RE_LABELS = re.compile('^([A-Z_.][A-Z_0-9]+)', re.IGNORECASE)
 
-def __get_all_lines(source):
+def _get_all_lines(source):
     """Load all the lines of the file."""
     f = open(source)
     lines = f.readlines()
@@ -43,110 +43,120 @@ def __get_all_lines(source):
 
     return lines
 
-def emit_error(line, col, message):
-    """Emit an error"""
-    sys.stderr.write("E [%d,%d] %s\n" % (line, col, message))
 
-def emit_warning(line, col, message):
-    """Emit an error"""
-    sys.stderr.write("W [%d,%d] %s\n" % (line, col, message))
-
-
-def check_load_syntax(lines):
-    """Check the syntax for loading registers"""
-
-    for i, line in enumerate(lines):
-        load_line = i+1
-        load_from_mem = re.match(RE_LOAD_REGISTER_SYNTAX, line)
-        
-        if load_from_mem:
-            register = load_from_mem.group(1)
-            emit_error(load_line, -1, 'Register %s not loaded' % register)
-
-
-def check_labels(lines, min_width=8, max_width=30):
-    """Check the validity of labels.
-      -  Duplicates
-      - Size
+class Z80_Parser(object):
+    """Parse a z80 source file.
+    The source file is not supposed to be assembled alone.
     """
 
-    latest_parent = ''
-    labels = []
-    for i, line in enumerate(lines):
-        label_line = i +1
-        label_res = re.match(RE_LABELS, line)
 
-        if label_res:
-            # Get the label value (manage internal labels)
-            label = label_res.group(1)
-            if label.startswith('.'):
-                real_label = latest_parent + label
-            else:
-                real_label = label
-                latest_parent = label
+    def __init__(self, fname):
+        """Parse the source file."""
 
-            # Check if exists
-            if real_label in labels:
-                emit_error(label_line, 1, 'Label %s already exists' \
-                           % real_label)
-            else:
-                labels.append(real_label)
+        lines = _get_all_lines(fname)
+        self._fname = fname
 
-            # Check size validity
-            if len(label) < min_width:
-                emit_warning(label_line, 1, 'Label %s too short' % label)
-            elif len(label) > max_width:
-                emit_warning(label_line, 1, 'Label %s too long' % label)
-
-def check_macros(lines):
-    """Verify syntax macro"""
-
-    opened_macro = 0
-    macro_line = -1
-    macro_names = []
+        self.check_macros(lines)
+        self.check_load_syntax(lines)
+        self.check_labels(lines)
 
 
-    for i, line in enumerate(lines):
-        macro_line = i+ 1
 
-        # macro opening detection
-        open_re = re.match(RE_OPEN_MACRO, line)
-        if open_re:
-            macro_name = open_re.group(2)
+    def emit_error(self, line, col, message):
+        """Emit an error (does not use the column value)"""
+        sys.stderr.write("%s:%d Error %s\n" % (self._fname, line, message))
 
-            # check for included macros
-            if opened_macro:
-                emit_error(macro_line, -1,
-                           'Macro %s opened inside another one (%s)' \
-                              %(macro_name, macro_names[-1]))
-
-            # check for duplicated macros
-            if macro_name in macro_names:
-                emit_error(macro_line, -1,
-                           'Macro %s already defined' % macro_name)
-
-            opened_macro = opened_macro + 1
-            macro_names.append(macro_name)
-
-        close_re = re.match(RE_CLOSE_MACRO, line)
-        if close_re:
-            if not opened_macro:
-                emit_error(macro_line, -1, 'Macro not previously defined')
-            else:
-                opened_macro = opened_macro - 1
+    def emit_warning(self, line, col, message):
+        """Emit an error (does not use the column value)"""
+        sys.stderr.write("%s:%d Warning %s\n" % (self._fname, line, message))
 
 
-    if opened_macro:
-        emit_error(macro_line, -1, 'Macro %s never closed' % macro_names[-1])
+    def check_load_syntax(self, lines):
+        """Check the syntax for loading registers"""
 
-def check_source_file(source):
-    """Check the syntax of the source file."""
+        for i, line in enumerate(lines):
+            load_line = i+1
+            load_from_mem = re.match(RE_LOAD_REGISTER_SYNTAX, line)
+            
+            if load_from_mem:
+                register = load_from_mem.group(1)
+                self.emit_error(load_line, max(line.find('LD'), line.find('ld')), 'Register %s not loaded' % register)
 
-    lines = __get_all_lines(source)
-    check_macros(lines)
-    check_load_syntax(lines)
-    check_labels(lines)
 
+    def check_labels(self, lines, min_width=8, max_width=30):
+        """Check the validity of labels.
+          -  Duplicates
+          - Size
+        """
+
+        latest_parent = ''
+        labels = []
+        for i, line in enumerate(lines):
+            label_line = i +1
+            label_res = re.match(RE_LABELS, line)
+
+            if label_res:
+                # Get the label value (manage internal labels)
+                label = label_res.group(1)
+                if label.startswith('.'):
+                    real_label = latest_parent + label
+                else:
+                    real_label = label
+                    latest_parent = label
+
+                # Check if exists
+                if real_label in labels:
+                    self.emit_error(label_line, 1, 'Label %s already exists' \
+                               % real_label)
+                else:
+                    labels.append(real_label)
+
+                # Check size validity
+                if len(label) < min_width:
+                    self.emit_warning(label_line, 1, 'Label %s too short' % label)
+                elif len(label) > max_width:
+                    self.emit_warning(label_line, 1, 'Label %s too long' % label)
+
+    def check_macros(self, lines):
+        """Verify syntax macro"""
+
+        opened_macro = 0
+        macro_line = -1
+        macro_names = []
+
+
+        for i, line in enumerate(lines):
+            macro_line = i+ 1
+
+            # macro opening detection
+            open_re = re.match(RE_OPEN_MACRO, line)
+            if open_re:
+                macro_name = open_re.group(2)
+
+                # check for included macros
+                if opened_macro:
+                    self.emit_error(macro_line, -1,
+                               'Macro %s opened inside another one (%s)' \
+                                  %(macro_name, macro_names[-1]))
+
+                # check for duplicated macros
+                if macro_name in macro_names:
+                    self.emit_error(macro_line, -1,
+                               'Macro %s already defined' % macro_name)
+
+                opened_macro = opened_macro + 1
+                macro_names.append(macro_name)
+
+            close_re = re.match(RE_CLOSE_MACRO, line)
+            if close_re:
+                if not opened_macro:
+                    self.emit_error(macro_line, -1, 'Macro not previously defined')
+                else:
+                    opened_macro = opened_macro - 1
+
+
+        if opened_macro:
+            self.emit_error(macro_line, -1, 'Macro %s never closed' % macro_names[-1])
 
 # code
 if __name__ == '__main__':
@@ -160,7 +170,8 @@ if __name__ == '__main__':
         sys.stderr.write("%s does not exist\n" % source)
         quit(-1)
 
-    check_source_file(source)
+    
+    Z80_Parser(source)
 
 
 # metadata
